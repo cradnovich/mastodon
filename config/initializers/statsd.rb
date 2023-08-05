1e1bc646b1beb07b8b64a24bd6c8c5d3399a69c3
Meir Radnovich <meir@radnovich.com>
2023-08-05 23:14:53 +0300

    Removed nsa, bumped rails to 7.0.6

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -6,10 +6,10 @@ if ENV['STATSD_ADDR'].present?
   $statsd = ::Statsd.new(host, port)
   $statsd.namespace = ENV.fetch('STATSD_NAMESPACE') { ['Mastodon', Rails.env].join('.') }
 
-  ::NSA.inform_statsd($statsd) do |informant|
-    informant.collect(:action_controller, :web)
-    informant.collect(:active_record, :db)
-    informant.collect(:active_support_cache, :cache)
-    informant.collect(:sidekiq, :sidekiq)
-  end
+  # ::NSA.inform_statsd($statsd) do |informant|
+  #   informant.collect(:action_controller, :web)
+  #   informant.collect(:active_record, :db)
+  #   informant.collect(:active_support_cache, :cache)
+  #   informant.collect(:sidekiq, :sidekiq)
+  # end
 end
